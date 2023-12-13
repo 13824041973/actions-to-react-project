bd71f79e2029c5760f788ae890c8e5911f166ea4
刘耀龙 <1402098743@qq.com>
2023-12-13 10:29:00 +0800

    feat:项目配置redux，使用普通方式与RTK方式配置

---
+++ src/store/modules/entire/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+    entire: '222'
+}
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case 'xxx':
+
+            break;
+
+        default:
+            return state
+    }
+}
+
+export default reducer
