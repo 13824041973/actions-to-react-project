e585ea19bdbf2d6994137b46097d5ad422a3bf55
刘耀龙 <1402098743@qq.com>
2023-12-20 10:24:06 +0800

    feat：封装每个房间展示组件里的tabs

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -7,11 +7,14 @@ import { HomeWrapper } from "./style";
 import HomeSectionV1 from "./c-cpns/home-section-v1";
 import SectionHeader from "@/components/section-header";
 import SectionRooms from "@/components/section-rooms";
+import SectionTabs from "@/components/section-tabs";
 
 const Home = memo(() => {
   const goodPriceInfo = useSelector((state) => state.home.goodPriceInfo);
   const highScoreInfo = useSelector((state) => state.home.highScoreInfo);
   const discountInfo = useSelector((state) => state.home.discountInfo);
+
+  const tabNames = discountInfo?.dest_address?.map((item) => item.name);
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -28,6 +31,9 @@ const Home = memo(() => {
             title={discountInfo.title}
             subtitle={discountInfo.subtitle}
           />
+
+          <SectionTabs tabNames={tabNames} />
+
           <SectionRooms
             roomList={discountInfo.dest_list?.["佛山"]}
             itemWidth="33%"
